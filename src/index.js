d17b1159babdad69d58003d397f6ca45db81af67
Aaron Xavier <aarondxavier@gmail.com>
2018-07-20 16:34:52 -0700

    Redux persist boilerplate created and verified that store is rehydrated across sessions

--- src/index.js
+++ src/index.js
@@ -2,20 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import reducers from './reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
 const middlewares = [thunk, logger];
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = composeEnhancers(applyMiddleware(...middlewares))(createStore);
+const store = composeEnhancers(applyMiddleware(...middlewares))(createStore)(
+  persistedReducer
+);
+let persistor = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={store(reducers)}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
